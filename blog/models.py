891802d86a4f55cdd4110ef8aef2839d441f5449
Ivan Dorofeev <axxel123@rambler.ru>
2022-09-13 08:53:14 +0300

    Поправил модели под Django-codestyle

--- blog/models.py
+++ blog/models.py
@@ -25,8 +25,6 @@ class PostQuerySet(models.QuerySet):
 
 
 class Post(models.Model):
-    objects = PostQuerySet.as_manager()
-
     title = models.CharField('Заголовок', max_length=200)
     text = models.TextField('Текст')
     slug = models.SlugField('Название в виде url', max_length=200)
@@ -48,16 +46,18 @@ class Post(models.Model):
         related_name='posts',
         verbose_name='Теги')
 
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', args={'slug': self.slug})
+    objects = PostQuerySet.as_manager()
 
     class Meta:
         ordering = ['-published_at']
         verbose_name = 'пост'
         verbose_name_plural = 'посты'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args={'slug': self.slug})
 
 
 class TagQuerySet(models.QuerySet):
@@ -68,9 +68,14 @@ class TagQuerySet(models.QuerySet):
 
 
 class Tag(models.Model):
+    title = models.CharField('Тег', max_length=20, unique=True)
+
     objects = TagQuerySet.as_manager()
 
-    title = models.CharField('Тег', max_length=20, unique=True)
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
 
     def __str__(self):
         return self.title
@@ -80,11 +85,6 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag_filter', args={'tag_title': self.slug})
-
-    class Meta:
-        ordering = ['title']
-        verbose_name = 'тег'
-        verbose_name_plural = 'теги'
 
 
 class Comment(models.Model):
@@ -101,10 +101,10 @@ class Comment(models.Model):
     text = models.TextField('Текст комментария')
     published_at = models.DateTimeField('Дата и время публикации')
 
-    def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
-
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f'{self.author.username} under {self.post.title}'
